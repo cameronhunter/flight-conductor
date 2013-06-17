24dbd634821b75026070307b31f4498bd7e1dcaa
Cameron Hunter <hello@cameronhunter.co.uk>
2013-06-16 22:17:34 -0700

    Initial implementation

--- test/spec/flight-conductor.spec.js
+++ test/spec/flight-conductor.spec.js
@@ -11,8 +11,4 @@ describeComponent('lib/flight-conductor', function () {
     expect(this.component).toBeDefined();
   });
 
-  it('should do something', function () {
-    expect(true).toBe(false);
-  });
-
 });
